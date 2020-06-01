9aeda7c031fe43e1143ec7bd42d72a1bc5b6a9f2
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-01 21:34:46 +0430

    designed some menus graphically.

--- src/View/RegisterAndLoginView/Register/RegisterManager.java
+++ src/View/RegisterAndLoginView/Register/RegisterManager.java
@@ -1,13 +1,26 @@
 package View.RegisterAndLoginView.Register;
 
+import Controller.RegisterAndLogin;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class RegisterManager {
     private static Stage stage;
+    public TextField usernameField;
+    public PasswordField passwordField;
 
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
+    public void login() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        try {
+            RegisterAndLogin.getInstance().login(username, password);
+        } catch (Exception e) {
 
+        }
+    }
 }
