91440590b7efc918d3e27406c5c39b540f774587
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-02 19:03:10 +0430

    Implemented ChangePasswordManager

--- src/View/AccountView/ChangePasswordView/ChangePasswordManager.java
+++ src/View/AccountView/ChangePasswordView/ChangePasswordManager.java
@@ -1,7 +1,20 @@
 package View.AccountView.ChangePasswordView;
 
+import Controller.Account;
 import View.RegisterAndLoginView.MenuManager;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 
 public class ChangePasswordManager extends MenuManager {
+    public PasswordField passwordField;
+    public Label successMessage;
+    public Button changePasswordButton;
 
+    public void changePassword() {
+        Account.getInstance().changePassword(passwordField.getText());
+        passwordField.setDisable(true);
+        changePasswordButton.setDisable(true);
+        successMessage.setText("You have successfully changed tour password!");
+    }
 }
