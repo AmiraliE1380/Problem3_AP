c5c4c3209677d3bb80885dea8d2829d423cd4a21
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-08 00:22:46 +0430

    las commit

--- src/View/GameView/GameManager.java
+++ src/View/GameView/GameManager.java
@@ -10,6 +10,7 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
